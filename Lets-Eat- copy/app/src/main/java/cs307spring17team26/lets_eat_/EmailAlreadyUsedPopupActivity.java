b7b48b25ce0917d6934df5ac28b8b36aa0cbac4b
Nathan Chang <nathanchang@pal-nat186-159-35.itap.purdue.edu>
2017-02-25 13:50:09 -0500

    little modification to allow code to compile

--- Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/EmailAlreadyUsedPopupActivity.java
+++ Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/EmailAlreadyUsedPopupActivity.java
@@ -114,7 +114,7 @@ public class EmailAlreadyUsedPopupActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        findViewById(R.id.button).setOnTouchListener(mDelayHideTouchListener);
     }
 
     @Override
