4b6ddb60200b3927d7bb6ec3d0c835be7ec91151
sokolowski123 <sokola@purdue.edu>
2017-03-23 18:20:04 -0400

    Update ResponseParser.java

--- Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/ResponseParser.java
+++ Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/ResponseParser.java
@@ -2,78 +2,28 @@ package cs307spring17team26.lets_eat_;
 
 import java.util.Arrays;
 
-import org.json.JSONObject;
+
+import org.bson.Document;
 
 import com.mongodb.*;
 
 public class ResponseParser {
-	
-	Account parseAccount(JSONObject user) {
+
+	Account parseAccount(Document user) {
 		String email = user.get("_id").toString();
 		String pass = user.get("_pass").toString();
 		return new Account(email, pass);
 	}
 	
-	String parseMessage(JSONObject user) {
-		String message = user.get("_chat").toString();
-		//System.out.println(message);
-		return "";
-	}
-	
-	User parseUser(JSONObject user) {
-		String dob = user.get("dob").toString();
-		String name = user.get("name").toString();
-		String userId = user.get("userId").toString();
-		String gender = user.get("gender").toString();
-		String matches = user.get("_matches").toString();
-		String valid = user.getString("isValid").toString();
-		String blocked = user.get("blocked").toString();
-		String chat = user.getString("_chat").toString();
-		
-		matches = matches.substring(1, matches.length()-1);
-		blocked = blocked.substring(1, blocked.length()-1);
-		valid = valid.substring(1, valid.length()-1);
-		chat = chat.substring(1, chat.length()-1);
-	
-		String[] matchArray = matches.split(", ");
-		String[] validArray = valid.split(", ");
-		String[] blockedArray = blocked.split(", ");
-		String[] chatArray = chat.split("],");
-		for (int i = 0; i < chatArray.length-1; i++) {
-			chatArray[i] = chatArray[i] + "]";
-		}
-		String[][] chatHist = new String[chatArray.length][];
-		for (int i = 0; i < chatArray.length; i++) {
-			String messages = chatArray[i].substring(1, chatArray[i].length()-1);
-			String[] nextHist = messages.split(",");
-			chatHist[i] = new String[nextHist.length];
-			for (int j = 0; j < nextHist.length; j++) {
-				chatHist[i][j] = nextHist[j];
-			}
-		}
-		System.out.println(Arrays.deepToString(chatHist));
-		
-	
-		Match[] matchObjs = new Match[matchArray.length];
-		for (int i = 0; i < matchObjs.length; i++) {
-			boolean validB, blockedB;
-			if (validArray[i].equals("0")) {
-				validB = false;
-			}
-			else {
-				validB = true;
-			}
-			if (blockedArray[i].equals("0")) {
-				blockedB = false;
-			}
-			else {
-				blockedB = true;
-			}
-			matchObjs[i] = new Match(validB, blockedB, user.get("_id").toString(), matchArray[i]);
-			
-		}
-		
-		return new User(dob, userId, name, gender, matchObjs, chatHist);
+	User parseUser(Document user) {
+		int age = (Integer) user.get("age");
+	    	String userId = (String) user.get("userId");
+	    	String name = (String) user.get("name");
+	    	String gender = (String) user.get("gender");
+	    	String bio = (String) user.get("bio");
+	    	int[] location = (int[]) user.get("location");
+	    	int maxRange = (Integer) user.get("maxRange");
+	    	return new User(age, userId, name, gender, bio, location, maxRange);
 		
 	}
 	
