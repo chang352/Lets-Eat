f469030a60f3e646e42dd645664fe3e556c95db6
Nathan Chang <nathan.chang@comcast.net>
2017-04-07 09:59:38 -0400

    Update ProfileMatches.java

--- Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/ProfileMatches.java
+++ Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/ProfileMatches.java
@@ -22,6 +22,7 @@ public class ProfileMatches extends AppCompatActivity {
         FloatingActionButton deleteButton = (FloatingActionButton)findViewById(R.id.deleteButton);
         FloatingActionButton closeProfileButton = (FloatingActionButton)findViewById(R.id.closeProfileButton);
         FloatingActionButton chatButton = (FloatingActionButton)findViewById(R.id.chatButton);
+        FloatingActionButton restaurantButton = (FloatingActionButton)findViewById(R.id.restaurantButton);
 
         //removing the match, closing the activity and going back to match UI page
         deleteButton.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +41,14 @@ public class ProfileMatches extends AppCompatActivity {
             }
         });
 
+        //open restaurant UI page
+        restaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Restaurants r = new Restaurants();
+            }
+        });
+
         //chat with match
         chatButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,11 +61,12 @@ public class ProfileMatches extends AppCompatActivity {
 
         String name = "'Name'";
         String age = "'Age'";
-        String location = "'Location'";
-        String favCuisine = "'Favorite Cuisine'";
-        String school = "'School'";
+        //String location = "'Location'";
+        //String favCuisine = "'Favorite Cuisine'";
+        //String school = "'School'";
+        String gender = "'Gender'";
         String aboutMe = "'About Me'";
-        String[] info = {name, age, location, favCuisine, school, aboutMe};
+        String[] info = {name, age, gender, aboutMe};
         ArrayAdapter<String> infoListViewAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, info);
         infoListView.setAdapter(infoListViewAdapter);
 
