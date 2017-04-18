5dd2d25a136702b1950f4614ea0f6952e56f8985
Nathan Chang <nathan.chang@comcast.net>
2017-04-18 09:59:28 -0400

    ability to show matches in UI

--- Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/TabMatchesFragment.java
+++ Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/TabMatchesFragment.java
@@ -4,7 +4,9 @@ package cs307spring17team26.lets_eat_;
  * Created by nathanchang on 2/22/17.
  */
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,16 +17,24 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class TabMatchesFragment extends Fragment {
 
+    public TabMatchesFragment() {}
+
     private CharSequence email;
-    private String emailString;
-
-    public TabMatchesFragment() {
-        emailString = "";
-    }
-
     private ListView matchesListView;
+    private FloatingActionButton historyButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,25 +43,95 @@ public class TabMatchesFragment extends Fragment {
         Bundle account = this.getArguments();
         if (account!=null) {
             email = account.getCharSequence("email");
-            emailString = email.toString();
         }
 
-        SearchView searchBarSearchView = (SearchView)rootView.findViewById(R.id.searchBarSearchView); // initialize a search view
-        CharSequence query = searchBarSearchView.getQuery(); // get the query string currently in the text field
-        String[] info = {"Nathan Chang\n20", "MJ\n50", "Bill Nye\n60"};
-        matchesListView = (ListView)rootView.findViewById(R.id.matchesListView);
-        ArrayAdapter<String> infoListViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, info);
-        matchesListView.setAdapter(infoListViewAdapter);
-        matchesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        matchesListView = (ListView) rootView.findViewById(R.id.matchesListView);
+
+        final Context c = getActivity().getApplication();
+        final RequestQueue queue = Volley.newRequestQueue(c);
+        JsonObjectRequest j = new JsonObjectRequest(
+                Request.Method.GET, "http://ec2-52-24-61-118.us-west-2.compute.amazonaws.com/matches/" + email, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        JSONArray array = response.names();
+                        final String[] emailList = new String[array.length()];
+                        int j = 0;
+                        for (int i = 1; i < array.length(); i++) {
+                            try {
+                                if (response.getInt(array.getString(i)) == 2) {
+                                    emailList[j] = array.getString(i);
+                                    j++;
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        final String[] nameList = new String[emailList.length];
+                        getName(emailList, nameList, matchesListView);
+                    }
+                }, new Response.ErrorListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //show input text to change info
-                Intent intent = new Intent(getActivity(), ProfileMatches.class);
-                getActivity().startActivity(intent);
+            public void onErrorResponse(VolleyError error) {
+
             }
         });
+        queue.add(j);
         return rootView;
     }
-}
 
+    public void getName(final String[] emailArray, final String[] nameArray, final ListView matchesListView) {
+        Context c = getActivity().getApplication();
+        RequestQueue queue1 = Volley.newRequestQueue(c);
+        for (int k = 0; k < emailArray.length; k++) {
+            if (emailArray[k] == null) break;
+            final int finalK = k;
+            JsonObjectRequest r = new JsonObjectRequest(
+                    Request.Method.GET, "http://ec2-52-24-61-118.us-west-2.compute.amazonaws.com/users/" + emailArray[k], null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            getNameInRequest(response, nameArray, finalK);
+                            System.out.println(response);
+                            //nameArray[finalK] = response.getString("name");
+                            //getNameInRequest(response, nameArray, finalK);
+                            int l = 0;
+                            while (nameArray[l] != null) {
+                                l++;
+                            }
+                            String[] correct = new String[l];
+                            for (int m = 0; m < l; m++) {
+                                correct[m] = nameArray[m];
+                            }
+                            //searchesListView = (ListView)findViewById(R.id.searchesListView);
+                            final ArrayAdapter<String> matchListsViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, correct);
+                            //System.out.println(correct[0]);
+                            matchesListView.setAdapter(matchListsViewAdapter);
+                            matchesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Intent intent = new Intent(getActivity(), ProfileMatches.class);
+                                    intent.putExtra("email", emailArray[position]);
+                                    getActivity().startActivity(intent);
+                                }
+                            });
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+            queue1.add(r);
+        }
+    }
+
+    public void getNameInRequest(JSONObject response, String[] nameChange, int num) {
+        try {
+            nameChange[num] = response.getString("name");
+            System.out.println(nameChange[num]);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
