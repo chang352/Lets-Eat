5d7a6f6d75d80a2427c8d9ded7992567ec1d5f7b
Nathan Chang <nathan.chang@comcast.net>
2017-04-18 10:02:07 -0400

    ability to see match's profile

--- Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/ProfileMatches.java
+++ Lets-Eat- copy/app/src/main/java/cs307spring17team26/lets_eat_/ProfileMatches.java
@@ -1,94 +1,32 @@
 package cs307spring17team26.lets_eat_;
 
-import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+//import java.util.ArrayList;
+//import com.yelp.clientlib.entities.Business;
+
 public class ProfileMatches extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+    private CharSequence email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,48 +34,24 @@ public class ProfileMatches extends AppCompatActivity {
 
         setContentView(R.layout.profile_matches);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+        Bundle account = getIntent().getExtras();
+        if (account!=null) {
+            email = account.getCharSequence("email");
+        }
 
         ImageView imageView = (ImageView)findViewById(R.id.matchProfilePic);
-        ListView infoListView = (ListView)findViewById(R.id.matchInfo);
+        final ListView infoListView = (ListView)findViewById(R.id.matchInfo);
         FloatingActionButton deleteButton = (FloatingActionButton)findViewById(R.id.deleteButton);
         FloatingActionButton closeProfileButton = (FloatingActionButton)findViewById(R.id.closeProfileButton);
         FloatingActionButton chatButton = (FloatingActionButton)findViewById(R.id.chatButton);
-
-        String name = "'Name'";
-        String age = "'Age'";
-        String location = "'Location'";
-        String favCuisine = "'Favorite Cuisine'";
-        String school = "'School'";
-        String aboutMe = "'About Me'";
-
-        final JSONObject nameObj = new JSONObject();
-        try {
-            nameObj.put("toUser", name);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        FloatingActionButton restaurantButton = (FloatingActionButton)findViewById(R.id.restaurantButton);
+        TextView test = (TextView)findViewById(R.id.test);
 
         //removing the match, closing the activity and going back to match UI page
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // code for to remove match
-                SendMatchRequest sendMatchRequest = new SendMatchRequest();
-                Bundle account = getIntent().getExtras();
-                String currentUser = "";
-                if (account != null) {
-                    currentUser = account.getCharSequence("email").toString();
-                }
-                String toUser = "";
-                try {
-                    toUser = nameObj.getString("toUser");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                sendMatchRequest.denyMatch(currentUser, toUser, getApplication());
                 finish();
             }
         });
@@ -150,35 +64,46 @@ public class ProfileMatches extends AppCompatActivity {
             }
         });
 
+        //go to restaurants UI
+        restaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
         //chat with match
         chatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //code for to chat with match
-                SendMatchRequest sendMatchRequest = new SendMatchRequest();
-                Bundle account = getIntent().getExtras();
-                String currentUser = "";
-                if (account != null) {
-                    currentUser = account.getCharSequence("email").toString();
-                }
-                String toUser = "";
-                try {
-                    toUser = nameObj.getString("toUser");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                if (sendMatchRequest.canChat(currentUser, toUser, getApplication())) {
-                    //go to chat activity for the two
-                }
-                finish();
+                Intent intent = new Intent(ProfileMatches.this, ChatActivity.class);
+                startActivity(intent);
             }
         });
 
+        Context c = getApplication();
+        RequestQueue queue = Volley.newRequestQueue(c);
+        JsonObjectRequest j = new JsonObjectRequest(
+                Request.Method.GET, "http://ec2-52-24-61-118.us-west-2.compute.amazonaws.com/users/" + email, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String[] info = {response.getString("name"), Integer.toString(response.getInt("age")), response.getString("gender"), response.getString("bio")};
+                            ArrayAdapter<String> infoListViewAdapter = new ArrayAdapter<String>(ProfileMatches.this, android.R.layout.simple_list_item_1, info);
+                            infoListView.setAdapter(infoListViewAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-        String[] info = {name, age, location, favCuisine, school, aboutMe};
-        ArrayAdapter<String> infoListViewAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, info);
-        infoListView.setAdapter(infoListViewAdapter);
-
+            }
+        });
+        queue.add(j);
         // Set up the user interaction to manually show or hide the system UI.
         /*mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -191,58 +116,5 @@ public class ProfileMatches extends AppCompatActivity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
-
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 }
